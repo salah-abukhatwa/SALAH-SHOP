1bf25e38b8adaaeb896847f9e5a01a39fa0bf7b5
salah-abukhatwa <roben.gn2@gmail.com>
2025-08-30 22:03:00 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HSC76GM5.mjs";import"./chunk-U2FUNNQO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-X7UGPSP2.mjs";import"./chunk-U2FUNNQO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
