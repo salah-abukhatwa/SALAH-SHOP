b6f8173dfa7949ba5e9c5c6d2e45d15fcbe61569
salah-abukhatwa <roben.gn2@gmail.com>
2025-09-24 11:15:28 +0300

    change to  Railway backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://salah-ahop-backend.onrender.com/api', // your Render backend
+  apiUrl: 'https://salah-ahop-backend-production.up.railway.app/api', //  Railway backend
 };
